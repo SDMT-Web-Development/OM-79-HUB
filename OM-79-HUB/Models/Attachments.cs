915405b9c228c2c9d83a7cf920c2ce46d2a9b7f7
JohnCookWV <John.D.Cook@wv.gov>
2023-12-13 12:05:07 -0500

    nice

---
+++ OM-79-HUB/Models/Attachments.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OM79.Models.DB
+{
+    public class Attachments
+    {
+        [Key]
+        public int AttachmentID { get; set; }
+        public string? FileName { get; set; }
+        public string? FilePath { get; set; }
+        [ForeignKey("SubmissionID")]
+        public int? SubmissionID { get; set; }
+        public OMTable? Submission { get; set; }
+    }
+}
