91ff72d8191c0113015c3a26b8f8c0a034b5671e
RichardT1994 <96494628+RichardT1994@users.noreply.github.com>
2024-04-16 10:46:49 -0400

    Added Admin Page

--- OM-79-HUB/Models/DB/OM79Hub/UserData.cs
+++ OM-79-HUB/Models/DB/OM79Hub/UserData.cs
@@ -8,16 +8,20 @@ namespace OM_79_HUB.Models.DB.OM79Hub
         public string? ENumber { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public bool? CRU { get; set; }
-        public bool? CRA { get; set; }
-        public bool? HDS { get; set; }
-        public bool? LRS { get; set; }
-        public bool? GISManager { get; set; }
-        public bool? Chief { get; set; }
-        public bool? DistrictReview { get; set; }
-        public int? District { get; set; }
-        public string? Email { get; set; }
-
-
-    }
+        public bool CRU { get; set; }
+        public bool CRA { get; set; }
+            public bool HDS { get; set; }
+            public bool LRS { get; set; }
+            public bool GISManager { get; set; }
+            public bool Chief { get; set; }
+            public bool DistrictReview { get; set; }
+            public int? District { get; set; }
+            public string? Email { get; set; }
+            public bool BridgeEngineer { get; set; }
+            public bool TrafficEngineer { get; set; }
+            public bool MaintenanceEngineer { get; set; }
+            public bool ConstructionEngineer { get; set; }
+            public bool RightOfWayManager { get; set; }
+            public bool DistrictManager { get; set; }
+     }
 }
