5a7089aef0676760bfe471eb27a6b4b69554fdee
Ethan-m-johnson <133664606+Ethan-m-johnson@users.noreply.github.com>
2024-05-23 11:12:57 -0400

    Finsihed file attachment system, download, delete upload, and confirmations

--- OM-79-HUB/Components/OMAttachmentViewComponent.cs
+++ OM-79-HUB/Components/OMAttachmentViewComponent.cs
@@ -1,23 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using OM79.Models.DB; // Update with the correct namespace
 using System.Linq;
+
+
 
 namespace OM_79_HUB.Components
 {
     public class OMAttachmentViewComponent : ViewComponent
     {
         private readonly OM79Context _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public OMAttachmentViewComponent(OM79Context context)
+        public OMAttachmentViewComponent(OM79Context context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
-        public IViewComponentResult Invoke(int hubId)
+        public IViewComponentResult Invoke(int OMItemID)
         {
-            var entries = _context.OMTable.Where(entry => entry.HubId == hubId).ToList();
-            return View("~/Views/OM79/_OMAttachment.cshtml", entries); // Specify the correct view path here
-        }
+            Console.WriteLine("-----------------------------------------------------------------------------------");
+            Console.WriteLine("---------------------------------" + OMItemID + "----------------------------------");
+            Console.WriteLine("-----------------------------------------------------------------------------------");
 
+            var baseDir = Path.Combine(_webHostEnvironment.WebRootPath, "OMAttachments");
+            var OMItemfolderDir = Path.Combine(baseDir, "OM79-" + OMItemID + "-Attachments");
+
+            List<string> files = new List<string>();
+            if (Directory.Exists(OMItemfolderDir))
+            {
+                files = Directory.GetFiles(OMItemfolderDir).Select(Path.GetFileName).ToList();
+            }
+
+            var viewModel = new OMAttachmentViewModel
+            {
+                DirectoryName = OMItemfolderDir,
+                Files = files,
+                OMITEMID = OMItemID // Assuming OMItemID is the HubId, update accordingly
+            };
+
+            return View("~/Views/OM79/_OMAttachment.cshtml", viewModel);
+        }
+    }
+
+    public class OMAttachmentViewModel
+    {
+        public string DirectoryName { get; set; }
+        public List<string> Files { get; set; }
+        public int OMITEMID { get; set; }
     }
 }
